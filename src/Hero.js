0b554868c3f7d4e731813f7e68b68b59dab6ae6e
frankjay <frankjr212711@gmail.com>
2024-02-14 16:02:05 +0000

    hie

--- src/Hero.js
+++ src/Hero.js
@@ -1,10 +1,38 @@
 
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react'
+// import 'swiper/swiper-bundle.min.css'
 
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/scrollbar';
 
 export const Hero = () => {
   return (
-    <div className='hero'>
-      skyHero
+    <div className="hero">
+
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+
+      >
+        <SwiperSlide>
+          <img src="images/sky-img-01.jpg" alt="" style={{ width: '100%' }} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/sky-img-02.jpg" alt="" style={{ width: '100%' }} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/sky-img-03.jpg" alt="" style={{ width: '100%' }} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/sky-img-04.jpg" alt="" style={{ width: '100%' }} />
+        </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
